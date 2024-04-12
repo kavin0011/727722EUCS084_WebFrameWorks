340d22ea0f71386c2753d7dc5762b89747fd1e54
kavin0011 <150165983+kavin0011@users.noreply.github.com>
2024-04-12 17:29:11 +0530

    Add files via upload

---
+++ Day2/ClassWork/CE3/src/main/java/com/example/ce3/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.ce3.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.ce3.model.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class StudentController {
+    @GetMapping("/student")
+    public Student getInfo() {
+        return new Student(1, "Kavin A", "This is a sample description");
+    }
+
+}
