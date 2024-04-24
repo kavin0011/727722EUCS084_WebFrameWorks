d3fbabb8884cb631169a2036c23d93d0b976d26c
kavin0011 <150165983+kavin0011@users.noreply.github.com>
2024-04-24 09:42:39 +0530

    Delete Day6 directory

--- Day6/Day 6/CE2/src/main/java/com/example/ce2/repository/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.ce2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ce2.model.Student;
- 
-public interface StudentRepo extends JpaRepository<Student,Integer> {
-    
-}
