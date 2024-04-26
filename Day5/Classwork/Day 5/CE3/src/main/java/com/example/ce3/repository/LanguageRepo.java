29f8a6b7040115fccf0b49a7ffff88b1c572c8bc
kavin0011 <150165983+kavin0011@users.noreply.github.com>
2024-04-26 12:43:59 +0530

    Delete Day5/Classwork directory

--- Day5/Classwork/Day 5/CE3/src/main/java/com/example/ce3/repository/LanguageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.ce3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ce3.model.Language;
-
-public interface LanguageRepo extends JpaRepository<Language, Integer> {
-    
-}
